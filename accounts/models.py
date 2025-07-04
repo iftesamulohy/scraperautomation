7504483aa3be48a1ed9ddb96df119da1e190460f
iftesamulohy <64037194+iftesamulohy@users.noreply.github.com>
2025-07-04 21:12:18 +0600

    model updated

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ScrapedItem(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.URLField()
+    details_link = models.URLField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
