511aa5c538b7d861e6ebd4753e6225672572c1d3
iftesamulohy <64037194+iftesamulohy@users.noreply.github.com>
2025-07-04 21:13:04 +0600

    admin updated

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from accounts.models import ScrapedItem
+
 # Register your models here.
+@admin.register(ScrapedItem)
+class ScrapedItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image', 'details_link', 'timestamp')
+    search_fields = ('name',)
+    ordering = ('-timestamp',)
